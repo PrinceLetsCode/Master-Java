729ea803f50b1045790ee5c20634e01856e43c17
Prince Kumar <pk754728@gmail.com>
2022-10-11 21:45:02 +0530

    adds new problems and their solutions with description

--- CodeChef/BuildingRace.java
+++ CodeChef/BuildingRace.java
@@ -1,2 +1,32 @@
-package CodeChef;public class BuildingRace {
+package CodeChef;
+
+import java.util.Scanner;
+
+public class BuildingRace {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        int T=scan.nextInt();
+
+        if(T<=0 || T>2500) return;
+
+        while(T-->0)
+        {
+            double A=scan.nextDouble();
+            double B=scan.nextDouble();
+            double X=scan.nextDouble();
+            double Y=scan.nextDouble();
+
+            double chef_time=A/X;
+            double chefina_time=B/Y;
+
+            if(chefina_time>chef_time) System.out.println("Chef");
+            else if(chefina_time<chef_time) System.out.println("Chefina");
+            else System.out.println("Both");
+        }
+    }
+
 }
--- CodeChef/SingleOperationPart1.java
+++ CodeChef/SingleOperationPart1.java
@@ -1,2 +1,29 @@
-package CodeChef;public class SingleOperationPart1 {
+package CodeChef;
+
+import java.util.Scanner;
+
+public class SingleOperationPart1 {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int T=scan.nextInt();
+
+        while(T-- >0)
+        {
+            int len=scan.nextInt();
+            String S=scan.next();
+
+            int res=len;
+            for (int i=1;i<len;i++ ) {
+                if(S.charAt(i)=='0')
+                {
+                    res=i;
+                    break;
+                }
+            }
+
+            System.out.println(res);
+        }
+    }
+
 }
