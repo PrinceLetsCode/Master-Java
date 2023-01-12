1e41a29dee3b76a57d33aa783b13fce162026be8
Prince Kumar <princekumarcodes@gmail.com>
2023-01-12 09:20:43 +0530

    adds new problems and their solutions

--- Recursion/HelloWorld.java
+++
@@ -1,2 +0,0 @@
-package Recursion;public class HelloWorld {
-}
--- leetCode/ListNode.java
+++ leetCode/ListNode.java
@@ -2,9 +2,9 @@ package leetCode;
 
 public class ListNode {
 
-      int val;
-      ListNode next;
+      public int val;
+      public ListNode next;
       ListNode() {}
-      ListNode(int val) { this.val = val; }
+      public ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
