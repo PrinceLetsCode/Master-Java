5c4dc9eb86c53a1bf30fcb1bb9b00938a3c77886
Prince Kumar <princekumarcodes@gmail.com>
2023-01-16 11:16:01 +0530

    adds new problem

--- leetCode/NearestPointToXandY.java
+++ leetCode/NearestPointToXandY.java
@@ -1,2 +1,45 @@
-package leetCode;public class NearestPointToXandY {
+package leetCode;
+
+public class NearestPointToXandY {
+    public static void main(String[] args) {
+        int[][] points = {{1, 2}, {3, 1}, {2, 4}, {2, 3}, {4, 4}};
+
+        int[] point = new int[]{nearestValidPoint(3, 4, points)};
+
+    }
+    public static int nearestValidPoint(int x, int y, int[][] points) {
+        int distance = Integer.MAX_VALUE;
+        int index=-1;
+        for (int i = 0; i < points.length; i++) {
+            if (x == points[i][0] || y == points[i][1]) {
+                int manh = Math.abs(x - points[i][0]) + Math.abs(y - points[i][1]);
+                if (manh < distance) {
+                    distance=manh;
+                    index = i;
+                }
+            }
+        }
+        return index;
+    }
+
+    public static int nearestValidPoint_optimized(int x, int y, int[][] points) {
+        int result = -1, minSum = Integer.MAX_VALUE;
+        for (int i = 0; i < points.length; i++) {
+            if (points[i][0] == x) {
+                int manh = Math.abs(y - points[i][1]);
+                if (minSum > manh) {
+                    minSum = manh;
+                    result = i;
+                }
+            } else if (points[i][1] == y) {
+                int manh = Math.abs(x - points[i][0]);
+                if (minSum > manh) {
+                    minSum = manh;
+                    result = i;
+                }
+            }
+        }
+        return result;
+    }
+
 }
--- leetCode/SignOfProductOFAnArray.java
+++ leetCode/SignOfProductOFAnArray.java
@@ -1,2 +1,19 @@
-package leetCode;public class SignOfProductOFAnArray {
+package leetCode;
+
+public class SignOfProductOFAnArray {
+    public static void main(String[] args) {
+        int[] nums = {9,72,34,29,-49,-22,-77,-17,-66,-75,-44,-30,-24};
+        System.out.println(arraySign(nums));
+    }
+
+    public static int arraySign(int[] nums) {
+        int minCount=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == 0) return 0;
+            if(nums[i]<0) minCount++;
+        }
+
+        return minCount%2==0?1:-1;
+
+    }
 }
